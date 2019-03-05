a4d2fd5ed7e7b3764a98d7de4a1d3e005ade42b3
Frantisek Sumsal <frantisek@sumsal.cz>
2019-03-05 13:50:28 +0100

    test: parallelize tasks in TEST-24-UNIT-TESTS

--- test/TEST-24-UNIT-TESTS/testsuite.sh
+++ test/TEST-24-UNIT-TESTS/testsuite.sh
@@ -4,31 +4,81 @@
 #set -ex
 #set -o pipefail
 
-for i in /usr/lib/systemd/tests/test-*; do
-    if [[ ! -x $i ]]; then continue; fi
-    NAME=${i##*/}
-    echo "Running $NAME"
-    $i > /$NAME.log 2>&1
-    ret=$?
-    if (( $ret && $ret != 77 )); then
-        echo "$NAME failed with $ret"
-        echo $NAME >> /failed-tests
-        echo "--- $NAME begin ---" >> /failed
-        cat /$NAME.log >> /failed
-        echo "--- $NAME end ---" >> /failed
-    elif (( $ret == 77 )); then
-        echo "$NAME skipped"
-        echo $NAME >> /skipped-tests
-        echo "--- $NAME begin ---" >> /skipped
-        cat /$NAME.log >> /skipped
-        echo "--- $NAME end ---" >> /skipped
-    else
-        echo "$NAME OK"
-        echo $NAME >> /testok
+CHUNK_SIZE="$(nproc)"
+IFS=$'\n' TEST_LIST=($(ls /usr/lib/systemd/tests/test-*))
+
+# Check test results
+# Arguments:
+#   $1: test path
+#   $2: test exit code
+function check_result() {
+    if [[ $# -ne 2 ]]; then
+        echo >&2 "check_result: missing arguments"
+        exit 1
     fi
 
-    systemd-cat echo "--- $NAME ---"
-    systemd-cat cat /$NAME.log
+    local name="${1##*/}"
+    local ret=$2
+
+    if [[ $ret -ne 0 && $ret != 77 ]]; then
+        echo "$name failed with $ret"
+        echo "$name" >> /failed-tests
+        {
+            echo "--- $name begin ---"
+            cat "/$name.log"
+            echo "--- $name end ---"
+        } >> /failed
+    elif [[ $ret == 77 ]]; then
+        echo "$name skipped"
+        echo "$name" >> /skipped-tests
+        {
+            echo "--- $name begin ---"
+            cat "/$name.log"
+            echo "--- $name end ---"
+        } >> /skipped
+    else
+        echo "$name OK"
+        echo "$name" >> /testok
+    fi
+
+    systemd-cat echo "--- $name ---"
+    systemd-cat cat "/$name.log"
+}
+
+base=0
+# Outer loop: run until $base exceeds or equals to the length of the TEST_LIST array
+while [[ $base -lt ${#TEST_LIST[@]} ]]; do
+    # Associative array for running tasks, where running[test-path]=PID
+    declare -A running=()
+
+    # Run CHUNK_SIZE tasks in parallel and save their PIDs into the aforementioned
+    # associative array
+    for (( chunk = 0; chunk < CHUNK_SIZE; chunk++ )); do
+        idx=$((base + chunk))
+        testcase="${TEST_LIST[$idx]}"
+        # This covers both non-executable tests and indexes beyond array length
+        if [[ -x $testcase ]]; then
+            log_file="/${testcase##*/}.log"
+            $testcase &> "$log_file" &
+            running[$testcase]=$!
+        fi
+    done
+
+    # Loop over all running tasks and wait for each of them to complete. After
+    # that check result of each task.
+    # This can be done in serialized fashion as we still have to wait for the
+    # longest running task anyway.
+    for key in "${!running[@]}"; do
+        wait ${running[$key]}
+        ec=$?
+        check_result "$key" $ec
+    done
+
+    # Increment the base counter by number of tasks which we just ran in parallel
+    base=$((base + CHUNK_SIZE))
+    # To properly clear the associative array we need to call unset before
+    # declare -A
+    unset running
 done
 
 exit 0
